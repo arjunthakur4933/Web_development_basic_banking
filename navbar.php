1f4ddd78728a67a36ca57c06451bd184918fc78e
arjunthakur4933 <arjunthakur4933@gmail.com>
2021-07-18 22:28:09 +0530

    first commit.

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light" >
+      <a class="navbar-brand" href="index.html" style="color:red">Bank of Professionals</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li  class="nav-item">
+                <a class="nav-link" href="index.php" style="color:white">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color:white">Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color:white">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color:white">Transaction History</a>
+              </li>
+          </div>
+       </nav>
